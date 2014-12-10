21436af073b94ff03d79184561264befc9b4c27b
Linden Krouse <ztaticnull@gmail.com>
2014-12-09 23:38:21 -0500

    Fixed raise/lower volume and made volume indication show if its muted

--- panel/.panel/showVol.sh
+++ panel/.panel/showVol.sh
@@ -1,3 +1,10 @@
 #!/bin/env sh
 
-ponymix get-volume | awk '{print $1"%"}' | dzen2 -w 50 -h 30 -x 20 -y 20 -p 1 -xs 1
+vol=`ponymix get-volume | awk '{print $1"%"}'`
+
+ponymix is-muted
+if [ $? == 0 ]; then
+  vol="Muted"
+fi
+
+echo $vol | dzen2 -w 50 -h 30 -x 20 -y 20 -p 1 -xs 1
