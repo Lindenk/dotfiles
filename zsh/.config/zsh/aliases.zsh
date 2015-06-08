cb2a5a6e437a99c20973a586d7866dc37d140379
Linden Krouse <ztaticnull@gmail.com>
2015-06-08 00:22:43 -0400

    Fixed grep options

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias ls='ls --color=auto'
 alias la='ls -al --color=auto'
 alias ll='ls -l --color=auto'
 alias sl='ls --color=auto'
+
+# grep
+alias grep='grep --color=auto'
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -6,7 +6,7 @@ export TERM=xterm-256color
 export CLICOLOR=1
 
 # Enable color in grep
-export GREP_OPTIONS='--color=auto'
+#export GREP_OPTIONS='--color=auto'
 #export GREP_COLOR='3;33'
 
 # Editor
