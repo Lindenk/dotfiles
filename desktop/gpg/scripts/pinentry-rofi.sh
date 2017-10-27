9b76517d87033a5c5228256413b832e415c44fcb
Linden Krouse <ztaticnull@gmail.com>
2017-10-27 01:25:47 -0400

    Added public signing key to gitconfig, and added a rofi theme

--- desktop/gpg/scripts/pinentry-rofi.sh
+++ desktop/gpg/scripts/pinentry-rofi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "OK"
-#echo "$PINENTRY_USER_DATA" >> /home/lindenk/test.log
+echo "$PINENTRY_USER_DATA" >> /home/lindenk/test.log
 while read cmd rest; do
     cmd=$(printf "%s" "$cmd" | tr 'A-Z' 'a-z')
     if [ -z "$cmd" ]; then
@@ -11,7 +11,7 @@ while read cmd rest; do
         \#*)
         ;;
         getpin)
-            _PP=$(echo "" | DISPLAY=:0 rofi -dmenu -password -p "Passphrase: ")
+            _PP=$(echo "" | DISPLAY=:0 rofi -dmenu -password -p "Passphrase: " -theme purple)
             echo "D $_PP"
             echo "OK"
             ;;
