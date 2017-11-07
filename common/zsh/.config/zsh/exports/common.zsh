7bd419f53831d4b4b398807beebcc7f8fe274416
Linden Krouse <ztaticnull@gmail.com>
2017-11-07 16:40:04 -0500

    Added todoist and XDG env vars

--- common/zsh/.config/zsh/exports/common.zsh
+++ common/zsh/.config/zsh/exports/common.zsh
@@ -35,14 +35,14 @@ export RUST_SRC_PATH='/usr/src/rust/src'
 export GOPATH=$(echo ~/.go)
 
 # XDG
-#export XDG_DESKTOP_DIR="$HOME/"
-#export XDG_DOCUMENTS_DIR="$HOME/documents/"
-#export XDG_DOWNLOAD_DIR="$HOME/downloads/"
-#export XDG_MUSIC_DIR="$HOME/music/"
-#export XDG_PICTURES_DIR="$HOME/pictures/"
-#export XDG_VIDEOS_DIR="$HOME/videos/"
-#export XDG_CONFIG_HOME="$HOME/.config/"
-#export XDG_DATA_HOME="$HOME/.local/share/"
+export XDG_DESKTOP_DIR="$HOME/"
+export XDG_DOCUMENTS_DIR="$HOME/documents/"
+export XDG_DOWNLOAD_DIR="$HOME/downloads/"
+export XDG_MUSIC_DIR="$HOME/music/"
+export XDG_PICTURES_DIR="$HOME/pictures/"
+export XDG_VIDEOS_DIR="$HOME/videos/"
+export XDG_CONFIG_HOME="$HOME/.config/"
+export XDG_DATA_HOME="$HOME/.local/share/"
 
 # Other
 DATA_DIR="$HOME/.local/share"
