0b406fc2cb9fe6b1aace9fbbc3d78ff29b74341b
Linden Krouse <ztaticnull@gmail.com>
2021-05-13 14:36:23 -0400

    Updated a few scripts

--- common/scripts/ssh-crossover.sh
+++ common/scripts/ssh-crossover.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ping -6 -c 1 FF02::1%enp6s0 | head -2 | tail -1 | awk '{print substr($4, 1, length($4)-1)}' | xargs ssh $@
+INTERFACE=`ip addr | sed -rn 's/^[0-9]+: (e(\w)+):.*/\1/p' | head -1`
+
+MY_ADDRESS=`ip addr show enp109s0f1 | grep -e 'inet6' | grep -e 'link' | awk '{print $2}' | sed -r 's|/.+||'`
+
+OTHER_ADDRESS=`ping -6 -c 2 FF02::1%$INTERFACE | head -3 | tail -2 | awk '{print substr($4, 1, length($4)-1)}' | sed "/$MY_ADDRESS/d"`
+
+ssh "$OTHER_ADDRESS" $@
